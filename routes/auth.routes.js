0a2aa63133de0f1218f8ab4276fa8e527929883b
Victor Almeida <victor.a.sa21@gmail.com>
2022-11-28 20:49:34 +0100

    fix signup validation

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,45 +1,45 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 // ℹ️ Handles password encryption
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
 
 // How many rounds should bcrypt run the salt (default - 10 rounds)
 const saltRounds = 10;
 
 // Require the User model in order to interact with the database
-const User = require("../models/User.model");
+const User = require('../models/User.model');
 
 //middleware
-const { isLoggedOut, isLoggedIn } = require("../middleware/route-guard.js");
+const { isLoggedOut, isLoggedIn } = require('../middleware/route-guard.js');
 
 // GET /auth/signup FORM
-router.get("/auth/signup", isLoggedOut, (req, res) => {
-  res.render("auth/signup");
+router.get('/auth/signup', isLoggedOut, (req, res) => {
+  res.render('auth/signup');
 });
 
 // POST /auth/signup CREATE USER
-router.post("/auth/signup", async (req, res) => {
+router.post('/auth/signup', async (req, res) => {
   const { username, password } = req.body;
   // Check that username, email, and password are provided
-  if (username === "" || password === "") {
-    res.status(400).render("auth/signup", {
+  if (username === '' || password === '') {
+    res.status(400).render('auth/signup', {
       errorMessage:
-        "All fields are mandatory. Please provide your username and password.",
+        'All fields are mandatory. Please provide your username and password.',
     });
     return;
   }
   if (password.length < 6) {
-    res.status(400).render("auth/signup", {
-      errorMessage: "Your password needs to be at least 6 characters long.",
+    res.status(400).render('auth/signup', {
+      errorMessage: 'Your password needs to be at least 6 characters long.',
     });
     return;
   }
   const userFromDB = await User.findOne({ username });
 
   if (userFromDB !== null) {
-    res.render("auth/signup", {
-      errorMessage: "This Username already exists!",
+    res.render('auth/signup', {
+      errorMessage: 'This Username already exists!',
     });
     return;
   } else {
@@ -51,44 +51,44 @@ router.post("/auth/signup", async (req, res) => {
       password: hashedPassword,
     });
 
-    res.redirect("/login");
+    res.redirect('/login');
   }
 });
 
 //LOGIN GET /auth/LOGIN FORM
-router.get("/login", (req, res) => res.render("auth/login"));
+router.get('/login', (req, res) => res.render('auth/login'));
 
 //LOGIN POST /auth/LOGIN GET EXISTING USER FROM DB
-router.post("/login", (req, res, next) => {
+router.post('/login', (req, res, next) => {
   const { username, password } = req.body;
 
-  if (username === "" || password === "") {
-    res.status(400).render("auth/login", {
+  if (username === '' || password === '') {
+    res.status(400).render('auth/login', {
       errorMessage:
-        "All fields are mandatory. Please provide your username and password.",
+        'All fields are mandatory. Please provide your username and password.',
     });
     return;
   }
 
   User.findOne({ username }).then((userFromDB) => {
     if (!userFromDB) {
-      res.render("auth/login", { errorMessage: " Oops!! Wrong credentials" });
+      return res.render('auth/login', {
+        errorMessage: ' Oops!! Wrong credentials',
+      });
     }
     if (bcrypt.compareSync(password, userFromDB.password)) {
       req.session.User = userFromDB;
-      res.redirect("/dashboard");
+      res.redirect('/dashboard');
     } else
-      res.render("auth/login", { errorMessage: " Oops!! Wrong credentials" });
+      res.render('auth/login', { errorMessage: ' Oops!! Wrong credentials' });
   });
 });
 
-
-
 //POST  /logout
-router.get("/logout", (req, res, next) => {
+router.get('/logout', (req, res, next) => {
   // Logout user
   req.session.destroy();
-  res.redirect("/");
+  res.redirect('/');
 });
 
 module.exports = router;
